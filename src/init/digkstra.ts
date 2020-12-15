4137f13265cb573f90b254a6d3610fc6f17e581f
李韻晟 <dreak603@gmail.com>
2020-12-15 16:41:20 +0800

    chore: add file prefix

--- src/init/digkstra.ts
+++ src/init/digkstra.ts
@@ -1,4 +1,4 @@
-export const DIJKSTRA: DIJKSTRAInstance = require('../../build/Release/dijkstra');
+export const DIJKSTRA: DIJKSTRAInstance = require('../../build/Release/dijkstra.node');
 
 interface DIJKSTRAInstance {
     greet(key: string): string;
