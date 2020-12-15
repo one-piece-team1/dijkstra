618b78209bf476d5a2418fb69cfc39595c85d8bc
李韻晟 <dreak603@gmail.com>
2020-12-15 21:58:54 +0800

    feat: finish binary heap function

--- addons/binary-heap.cpp
+++ addons/binary-heap.cpp
@@ -35,6 +35,83 @@ public:
   bool IsHeapEmpty() { return (heap.size() <= 1); }
 };
 
-BinaryHeap::BinaryHeap(/* args */) {}
+int BinaryHeap::FindPosition(int node) {
+  int idx = 0;
+  for (int i = 1; i < heap.size(); i++) {
+    if (heap[i].element == node) {
+      idx = i;
+    }
+  }
+  return idx;
+}
 
-BinaryHeap::~BinaryHeap() {}
+void BinaryHeap::MinHeapInsert(int node, int key) {
+  heap.push_back(HeapNode(node, key));
+  DecreaseKey(node, key);
+}
+
+void BinaryHeap::DecreaseKey(int node, int newKey) {
+  int node_index = FindPosition(node);
+
+  if (newKey >= heap[node_index].key) {
+    std::cout << "new key is not smaller than current one\n";
+    return;
+  }
+  heap[node_index].key = newKey;
+
+  while (node_index > 1 && heap[GetParentNode(node_index)].key > heap[node_index].key)
+  {
+    swap(heap[node_index], heap[GetParentNode(node_index)]);
+    node_index = GetParentNode(node_index);
+  }
+}
+
+void BinaryHeap::swap(struct HeapNode &n1, struct HeapNode &n2) {
+  struct HeapNode temp = n1;
+  n1 = n2;
+  n2 = temp;
+}
+
+int BinaryHeap::ExractMin() {
+  if (IsHeapEmpty()) {
+    std::cout << "error: Heap is empty\n";
+    exit(-1);
+  }
+
+  int min = heap[1].element;
+  heap[1] = heap[heap.size() - 1];
+  heap.erase(heap.begin() + heap.size() - 1);
+  MinHeapify(1, (int)heap.size());
+  return min;
+}
+
+void BinaryHeap::BuildMinHeap(std::vector<int> array) {
+  for (int i = 0; i < array.size(); i++) {
+    heap[i + 1].element = i;
+    heap[i + 1].key = array[i];
+  }
+  for (int i = (int)heap.size() / 2; i >= 1; i--) {
+    MinHeapify(i, (int)heap.size() - 1);
+  }
+}
+
+void BinaryHeap::MinHeapify(int node, int length) {
+  int left = 2 * node;
+  int right = 2 * node + 1;
+  int smallest;
+
+  if (left <= length && heap[left].key < heap[node].key) {
+    smallest = left;
+  } else {
+    smallest = node;
+  }
+
+  if (right <= length && heap[right].key < heap[smallest].key) {
+    smallest = right;
+  }
+
+  if (smallest != node) {
+    swap(heap[smallest], heap[node]);
+    MinHeapify(smallest, length);
+  }
+}
